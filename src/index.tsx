27bfdc03afff9e746c9b7077160527a54f9669bf
synasapmob <sonn51280@gmail.com>
2026-03-10 22:51:30 +0700

    test color text

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 import { ViteReactSSG } from "vite-react-ssg/single-page";
 
-export const createRoot = ViteReactSSG(<>hello</>);
+export const createRoot = ViteReactSSG(
+  <>
+    <p className="text-red-500">Hello world</p>
+  </>,
+);
